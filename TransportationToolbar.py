d63c25305755c64ac485991530bd0687fc8d268f
Joel Graff <monograff76@gmail.com>
2018-02-07 12:49:38 -0600

    Removed print statements

--- TransportationToolbar.py
+++ TransportationToolbar.py
@@ -14,21 +14,21 @@ def onWorkbenchActivated():
     tb_trans_general = mw.findChild(QtGui.QToolBar, "Transportation")
     tb_trans_align = mw.findChild(QtGui.QToolBar, "Transportation alignment")
 
-    if tb_trans_align and last and active == "SketcherWorkbench":
-
+    if tb_trans_align and active == "SketcherWorkbench":
+        print "show alignment"
         tb_trans_general.hide()
         tb_trans_align.show()
 
         last = False
 
     elif active == "TransportationWorkbench":
-
+        print "hide alignment"
         tb_trans_general.show()
         tb_trans_align.hide()
         last = True
 
     else:
-
+        print "default"
         last = False
 
 mw.workbenchActivated.connect(onWorkbenchActivated)
