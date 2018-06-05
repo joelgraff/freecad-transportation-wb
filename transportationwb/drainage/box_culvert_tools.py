4bc4536b19ee847e3a8e42f8b6e26a97f8e64683
Graff <GRAFFJC@nt.dot.state.il.us>
2018-06-05 09:47:59 -0500

    added draft support for tapered ends, support for DocumentObjectGroup

--- transportationwb/drainage/box_culvert_tools.py
+++ transportationwb/drainage/box_culvert_tools.py
@@ -32,7 +32,6 @@ __url__ = "https://www.freecadweb.org"
 
 import BoxCulvert
 import FreeCAD as App
-import FreeCAD as Gui
 
 if App.Gui:
     import FreeCADGui as Gui
@@ -45,7 +44,7 @@ def create_1_cell_box():
     """
 
     #add a group for the box culvert structure
-    obj = App.ActiveDocument.addObject("App::FeaturePython", "BoxCulvert1Cell")
+    obj = App.ActiveDocument.addObject("App::DocumentObjectGroupPython", "BoxCulvert1Cell")
 
     obj.Label = translate("Transportation", "BoxCulvert1Cell")
 
@@ -55,13 +54,18 @@ def create_1_cell_box():
     lib_path = App.ConfigGet("UserAppData") + "Mod\\freecad-transportation-wb\\data\\drainage\\box_culvert1.FCStd"
 
     #get and validate the selected sweep path
-    sel = Gui.Selection.getSelection()[0]
+    sel = Gui.Selection.getSelection()
     valid_sel = None
 
-    if hasattr(sel, "Proxy"):
-        if hasattr(sel.Proxy, "Type"):
-            if sel.Proxy.Type == "Alignment"
-                valid_sel = sel
+    #abort if no selection
+    if not sel:
+        print("No alignment selected")
+        return None
+
+    if hasattr(sel[0], "Proxy"):
+        if hasattr(sel[0].Proxy, "Type"):
+            if sel[0].Proxy.Type == "Alignment":
+                valid_sel = sel[0].Group[0]
 
     if valid_sel is None:
         print ("Invalid selection for sweep")
@@ -71,8 +75,8 @@ def create_1_cell_box():
 
     #reference the sketch to sweep and orient it along the path
     box.attach_sketch(lib_path, "box_1_cell")
-    box.set_sketch_normal()
-    
+    box.orient_profile()
+
     App.ActiveDocument.recompute()
 
     #sweep the profile
@@ -82,34 +86,112 @@ def create_1_cell_box():
 
 def draft_ends():
     
-    sel = Gui.Selection.getSelection()
-
+    app_doc = App.ActiveDocument
+    gui_doc = Gui.ActiveDocument
+    gui_sel = Gui.Selection
+    faces = gui_sel.getSelectionEx()[0].SubElementNames
     face_count = 0
 
-    if len(sel) == 2:
+    #ensurce exactly two objects are selected
+    #and count the number of faces
+    if len(faces) == 2:
         for i in range(2):
-            if hasattr(sel[i], "ShapeType"):
-                if sel[i].ShapeType == 'Face'
-                    face_count += 1
+            if "Face" in faces[i]:
+                face_count += 1
 
+    #fail if both objects are not faces
     if face_count != 2:
-        print ("invalid selection")
+        print ("invalid face selection")
         return None
 
-    doc = App.ActiveDocument
-    
-    Gui.ActiveDocument.setActiveObject('pdbody', None)
-    Gui.ActiveView.setActiveObject('pdbody', doc.findObjects('PartDesign::Body')[0])
+    sel = gui_sel.getSelection()
 
-    doc.Draft.Base = (doc.)
->>> App.activeDocument().Draft.Base = (App.ActiveDocument.box_1_cell_Sweep,["Face1"])
->>> Gui.Selection.clearSelection()
->>> Gui.activeDocument().hide("box_1_cell_Sweep")
->>> App.ActiveDocument.recompute()
->>> Gui.activeDocument().setEdit('Draft', 0)
->>> Gui.Selection.clearSelection()
->>> Gui.ActiveDocument.Draft.ShapeColor=Gui.ActiveDocument.box_1_cell_Body.ShapeColor
->>> Gui.ActiveDocument.Draft.LineColor=Gui.ActiveDocument.box_1_cell_Body.LineColor
->>> Gui.ActiveDocument.Draft.PointColor=Gui.ActiveDocument.box_1_cell_Body.PointColor
->>> Gui.ActiveDocument.Draft.Transparency=Gui.ActiveDocument.box_1_cell_Body.Transparency
->>> Gui.ActiveDocument.Draft.DisplayMode=Gui.ActiveDocument.box_1_cell_Body.DisplayMode
+    #ensure selected faces are both on the same object
+    if sel[0].Name != sel[1].Name:
+        print ("Selected faces must share the same object")
+        return None
+
+    add_pipe = gui_sel.getSelection()[0]
+    body = None
+
+    #type-check the objects in the hierarchy
+    if add_pipe.TypeId == "PartDesign::AdditivePipe":
+        body = add_pipe.getParentGeoFeatureGroup()
+
+        if body.TypeId == "PartDesign::Body":
+            Gui.activeView().setActiveObject('pdbody', body)
+
+    if body is None:
+        print ("invalid object heirarchy.  Additive Pipe or Body not found.")
+        return None
+
+    
+    #perform draft
+    draft_object = body.newObject("PartDesign::Draft", "Draft_" + faces[0])
+    draft_object.Base = (add_pipe, [faces[0]])
+    gui_sel.clearSelection()
+
+    app_doc.recompute()
+
+    #Gui.activeDocument().setEdit('Draft', 0)
+    gui_draft = gui_doc.getObject(draft_object.Name)
+    gui_body = gui_doc.getObject(body.Name)
+
+    gui_draft.ShapeColor = gui_body.ShapeColor
+    gui_draft.LineColor = gui_body.LineColor
+    gui_draft.PointColor = gui_body.PointColor
+    gui_draft.Transparency = gui_body.Transparency
+    gui_draft.DisplayMode = gui_body.DisplayMode
+
+    draft_object.Angle = 30.0
+    draft_object.Reversed = 0
+    draft_object.NeutralPlane = (add_pipe, [faces[1]])
+    draft_object.PullDirection = None
+    draft_object.Base = (add_pipe, [faces[0]])
+
+    app_doc.recompute()
+    
+
+#-----------------------------------------------------------------
+#>>> App.activeDocument().box_1_cell001_Body.newObject("PartDesign::Draft","Draft")
+#>>> App.activeDocument().Draft.Base = (App.ActiveDocument.box_1_cell001_Sweep,["Face1"])
+#>>> Gui.Selection.clearSelection()
+#>>> Gui.activeDocument().hide("box_1_cell001_Sweep")
+#>>> App.ActiveDocument.recompute()
+#>>> Gui.activeDocument().setEdit('Draft', 0)
+#>>> Gui.Selection.clearSelection()
+#>>> Gui.ActiveDocument.Draft.ShapeColor=Gui.ActiveDocument.box_1_cell001_Body.ShapeColor
+#>>> Gui.ActiveDocument.Draft.LineColor=Gui.ActiveDocument.box_1_cell001_Body.LineColor
+#>>> Gui.ActiveDocument.Draft.PointColor=Gui.ActiveDocument.box_1_cell001_Body.PointColor
+#>>> Gui.ActiveDocument.Draft.Transparency=Gui.ActiveDocument.box_1_cell001_Body.Transparency
+#>>> Gui.ActiveDocument.Draft.DisplayMode=Gui.ActiveDocument.box_1_cell001_Body.DisplayMode
+#>>> App.ActiveDocument.Draft.Angle = 45.000000
+#>>> App.ActiveDocument.Draft.Reversed = 0
+#>>> App.ActiveDocument.Draft.NeutralPlane = (App.ActiveDocument.box_1_cell001_Sweep, ["Face8"])
+#>>> App.ActiveDocument.Draft.PullDirection = None
+#>>> App.ActiveDocument.Draft.Base = (App.ActiveDocument.box_1_cell001_Sweep,["Face1",])
+#>>> Gui.activeDocument().hide("box_1_cell001_Sweep")
+#>>> App.ActiveDocument.recompute()
+#>>> Gui.activeDocument().resetEdit()
+
+
+#>>> App.activeDocument().box_1_cell001_Body.newObject("PartDesign::Draft","Draft001")
+#>>> App.activeDocument().Draft001.Base = (App.ActiveDocument.Draft,["Face3"])
+#>>> Gui.Selection.clearSelection()
+#>>> Gui.activeDocument().hide("Draft")
+#>>> App.ActiveDocument.recompute()
+#>>> Gui.activeDocument().setEdit('Draft001', 0)
+#>>> Gui.Selection.clearSelection()
+#>>> Gui.ActiveDocument.Draft001.ShapeColor=Gui.ActiveDocument.box_1_cell001_Body.ShapeColor
+#>>> Gui.ActiveDocument.Draft001.LineColor=Gui.ActiveDocument.box_1_cell001_Body.LineColor
+#>>> Gui.ActiveDocument.Draft001.PointColor=Gui.ActiveDocument.box_1_cell001_Body.PointColor
+#>>> Gui.ActiveDocument.Draft001.Transparency=Gui.ActiveDocument.box_1_cell001_Body.Transparency
+#>>> Gui.ActiveDocument.Draft001.DisplayMode=Gui.ActiveDocument.box_1_cell001_Body.DisplayMode
+#>>> App.ActiveDocument.Draft001.Angle = 45.000000
+#>>> App.ActiveDocument.Draft001.Reversed = 0
+#>>> App.ActiveDocument.Draft001.NeutralPlane = (App.ActiveDocument.Draft, ["Face8"])
+#>>> App.ActiveDocument.Draft001.PullDirection = None
+#>>> App.ActiveDocument.Draft001.Base = (App.ActiveDocument.Draft,["Face3",])
+#>>> Gui.activeDocument().hide("Draft")
+#>>> App.ActiveDocument.recompute()
+#>>> Gui.activeDocument().resetEdit()
