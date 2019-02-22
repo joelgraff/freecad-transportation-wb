c13f7c112bcf782e8a8bf6c4af5a87be835d4f3b
Graff <GRAFFJC@nt.dot.state.il.us>
2019-02-22 15:26:20 -0600

    implemented left-hand spirals

--- transportationwb/corridor/alignment/HorizontalAlignment.py
+++ transportationwb/corridor/alignment/HorizontalAlignment.py
@@ -519,16 +519,22 @@ class _HorizontalAlignment(Draft._Wire):
 
         bearing_in = App.Vector(math.sin(bearing), math.cos(bearing))
 
+        curve_dir = 1.0
+
+        if angle < 0.0:
+            curve_dir = -1.0
+            angle = abs(angle)
+
         _Xc = ((length_mm**2) / (6.0 * radius_mm))
         _Yc = (length_mm - ((length_mm**3) / (40 * radius_mm**2)))
 
         _dY = App.Vector(bearing_in).multiply(_Yc)
-        _dX = App.Vector(bearing_in.y, -bearing_in.x, 0.0).multiply(_Xc)
+        _dX = App.Vector(bearing_in.y, -bearing_in.x, 0.0).multiply(curve_dir).multiply(_Xc)
 
         theta_spiral = length_mm/(2 * radius_mm)
         arc_start = start_coord.add(_dX.add(_dY))
         arc_coords = [arc_start]
-        arc_coords.extend(_HorizontalAlignment.discretize_arc(arc_start, bearing + theta_spiral, radius, angle - (2 * theta_spiral), interval, interval_type))
+        arc_coords.extend(_HorizontalAlignment.discretize_arc(arc_start, bearing + (theta_spiral * curve_dir), radius, curve_dir * (angle - (2 * theta_spiral)), interval, interval_type))
 
         if len(arc_coords) < 2:
             print('Invalid central arc defined for spiral')
@@ -545,17 +551,17 @@ class _HorizontalAlignment(Draft._Wire):
             _y = _len - ((_len**5) / (40 * (radius_mm ** 2) * (length_mm**2)))
 
             _dY = App.Vector(bearing_in).multiply(_y)
-            _dX = App.Vector(bearing_in.y, -bearing_in.x, 0.0).multiply(_x)
+            _dX = App.Vector(bearing_in.y, -bearing_in.x, 0.0).multiply(curve_dir).multiply(_x)
 
             points.append(start_coord.add(_dY.add(_dX)))
 
         points.extend(arc_coords)
 
-        exit_bearing = bearing + angle
+        exit_bearing = bearing + (angle * curve_dir)
         bearing_out = App.Vector(math.sin(exit_bearing), math.cos(exit_bearing), 0.0)
 
         _dY = App.Vector(bearing_out).multiply(_Yc)
-        _dX = App.Vector(-bearing_out.y, bearing_out.x, 0.0).multiply(_Xc)
+        _dX = App.Vector(-bearing_out.y, bearing_out.x, 0.0).multiply(curve_dir).multiply(_Xc)
 
         end_coord = points[-1].add(_dY.add(_dX))
 
@@ -572,7 +578,7 @@ class _HorizontalAlignment(Draft._Wire):
             _y = _len - ((_len ** 5) / (40 * (radius_mm ** 2) * (length_mm**2)))
 
             _dY = App.Vector(-bearing_out).multiply(_y)
-            _dX = App.Vector(bearing_out.y, -bearing_out.x, 0.0).multiply(_x)
+            _dX = App.Vector(bearing_out.y, -bearing_out.x, 0.0).multiply(curve_dir).multiply(_x)
 
             temp.append(end_coord.add(_dY.add(_dX)))
 
